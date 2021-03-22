b916351a8611f38b3b4c95995cb0a7146126baf0
ANSH <quantamstrike@gmail.com>
2021-03-23 00:03:37 +0530

    Finally Done!!!

--- app/src/main/java/com/example/javasosapp/Message.java
+++ app/src/main/java/com/example/javasosapp/Message.java
@@ -11,26 +11,27 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Message extends AppCompatActivity {
-SharedPreferences pref;
-TextView editText5;
-Button backBtnMessage, editData;
-String message;
-Intent i;
+    SharedPreferences pref;
+    TextView editText5;
+    Button backBtnMessage, editData;
+    String message;
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
         editText5 = findViewById(R.id.editText5);
         backBtnMessage = findViewById(R.id.backBtnMessage);
         editData = findViewById(R.id.editData);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message);
+
         pref = getSharedPreferences("myKey", Context.MODE_PRIVATE);
         message = pref.getString("Message", "");
         editText5.setText(message);
         backBtnMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               i = new Intent(Message.this, OnClickUserProfile.class);
-               startActivity(i);
+                i = new Intent(Message.this, OnClickUserProfile.class);
+                startActivity(i);
             }
         });
         editData.setOnClickListener(new View.OnClickListener() {
